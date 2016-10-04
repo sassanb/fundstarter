37ca692e95eb74c6c268ddd018ba29c118eb2364
Ubuntu <ubuntu@ip-172-31-42-133.us-west-2.compute.internal>
2016-10-04 18:55:08 +0000

    part1

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-var express = require('express')
-var app = express()
+// Part 1
+var http = require('http');
+var fs = require('fs');
+var filename = 'public/index.html';
+
+console.log("App is running on localhost:8080");
+     
+http.createServer(function(req,response){
+    var buff = fs.readFileSync('index.html', "utf8")
+    response.writeHead(200, {"Content-type":"text/html"})
+    response.write(buff)
+    response.end()
+}).listen(8080);
 
 
-app.set('port', (process.env.PORT || 8080))
-//app.use(express.static(__dirname + '/public'))
+//var http = require('http'),
+//     fs = require('fs');
 
-//__dirname returns the directory that the currently executing script is in.
+/*
+     fs.readFile('./public/index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }            console.log("App is running on localhost:8080");
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+       response.write(html);  
+        response.end();  
+    }).listen(8080);
+ });
+*/
 
-app.get('/', function(request, response) {
-    response.sendFile('public/index.html',{root:__dirname})
 
-/* sends an entire HTTP response to the client,                                                                                                                                     
- including headers and content,                                                                                                                                                     
- which is why you can only call once*/
-
-
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at :" + app.get('port'))
-})
